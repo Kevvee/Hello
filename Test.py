327d79461bb8677b9213b0fdecbfff852e2b70f3
Kevvee <Kevjoh.044@gmail.com>
2021-09-10 13:42:04 +0200

    ✨ fredag

--- Test.py
+++ Test.py
@@ -1,1 +1,11 @@
-print("Hej på dig din jävla idiot")
+print("Hejsan!")
+
+name = input("Vad heter du?")
+
+print(f"Hej {name}!")
+
+age = input("Hur gammal är du?")
+print(f"Är du {age} år gammal?")
+
+därför = input(f"Varför är du {age} år gammal?")
+print("Aight om du säger så...")
