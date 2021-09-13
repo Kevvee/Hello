1f8fc43850b743f67df780ac030a17e35764a7d1
Kevvee <Kevjoh.044@gmail.com>
2021-09-13 13:15:11 +0200

    ✅  Klart och betalat

--- Test.py
+++ Test.py
@@ -9,3 +9,35 @@ print(f"Är du {age} år gammal?")
 
 därför = input(f"Varför är du {age} år gammal?")
 print("Aight om du säger så....")
+
+tal1 = 12
+tal2 = float(input("Skriv ett tal:"))
+
+summa = tal1 + tal2
+print(f"Summan av {tal1} + {tal2} = {summa}")
+
+
+tal1 = 12
+tal2 = 12
+tal3 = 24
+tal4 = 15
+
+summa = tal1 + tal2
+
+
+
+summa = tal1 + tal2
+
+print(f"Summan blir {summa}")
+
+summa = tal1 - tal2
+
+print(f"Summan blir {summa}")
+
+summa = tal1 * tal3
+
+print(f"Summa blir {summa}")
+
+summa = tal4 / tal3
+
+print(f"Summa blir {summa}")
